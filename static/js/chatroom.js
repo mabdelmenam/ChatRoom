db78a8eb39dbfcc4088bca99aa71776502acd50a
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2020-12-01 22:20:02 -0500

    Created profanity check function in views.py Message if profanity is found, is displayed in HTML

--- static/js/chatroom.js
+++ static/js/chatroom.js
@@ -11,3 +11,47 @@
 //Create a function to filter out a username based on if there is profanity in that username
 //1a. If profanity is found, prompt user to re-enter their username.
 //1b. If profanity is not found, send the username to the function checking if usernames are in the database.
+var username = document.getElementById("userbox");
+username.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("main-btn").click();
+  }
+});
+
+function filter(){
+    var file = "http://localhost:5000/profanity_filter";
+
+    var obj = {
+        user: username.value
+    };
+
+    var finalData = JSON.stringify(obj);
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            //Checking profanity JSON sent back from profanity_filter
+            var usernameClean = JSON.parse(this.responseText);
+
+            if(usernameClean.profanity_check == 1){ //Profanity
+                profanity_text = document.getElementById("profanity_found");
+                profanity_text.innerHTML = "Username cannot be used.";
+                
+                setTimeout(function() {
+                    profanity_text.innerHTML = "";
+                }, 3500);
+                console.log("BAD WORD");
+            }
+            else{ //No Profanity
+                console.log("CLEAN");
+            }
+            console.log("Response Text: " , this.responseText);
+        }
+    };
+
+    xhttp.open("POST", file, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(finalData);
+}
--- views.py
+++ views.py
@@ -13,3 +13,24 @@ def index():
 @app.route('/chatroom') # Chat Room
 def chatroom():
     return render_template('chatroom.html')
+
+@app.route('/profanity_filter', methods=['GET', 'POST']) #Profanity Check
+def profanity():
+        #send username to this functiono
+        #return answer back to js as JSON
+    req_data = request.get_json()
+    username = req_data['user']
+
+    usernameCheck = {}
+
+    txt = username.lower()
+    with open("more_words2.txt") as f:
+            for line in f:
+                if txt.find(line.strip()) > -1:
+                        usernameCheck['profanity_check'] = 1
+                        #print(line.strip(), file=sys.stderr)
+                        break
+                else:
+                        usernameCheck['profanity_check'] = 0
+
+    return jsonify(usernameCheck)
